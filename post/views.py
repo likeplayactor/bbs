d8491ace8bcf8e421a789108fea349ba4eb72eac
likeplayactor <283405449@qq.com>
2018-12-07 14:39:32 +0800

    完成了帖子修改功能

--- post/views.py
+++ post/views.py
@@ -13,11 +13,21 @@ def create_post(request):
         post = Post.objects.create(title=title, content=content)
         return redirect('/post/read/?post_id=%s' % post.id)
     else:
-        return render(request, 'create_post.html', {})
+        return render(request, 'create_post.html')
 
 
 def edit_post(request):
-    return render(request, 'edit_post.html', {})
+    if request.method == "POST":
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(id=post_id)
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%s' % post.id)
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(id=post_id)
+        return render(request, 'edit_post.html', {'post': post})
 
 
 def read_post(request):
